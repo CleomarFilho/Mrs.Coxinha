f8c0c6b6258e0a97c78747275dbdbb96f6e70d92
Cleomar Filho <cleofilhog15@gmail.com>
2023-10-02 22:07:54 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import {Header} from './components/Header'
+import { NavBar } from './components/NavBar'
+
+function App() {
+  return (
+    <div className="app">
+      <NavBar/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,13 @@
+const NavBar = () =>{
+  
+  return (
+    <div className="navBar">
+      <p>Mrs.Coxinha</p>
+      <a href="">Produtos</a>
+      <a href="">Endereços</a>
+      <a href="">Fale Conosco</a>
+      
+    </div>
+  )
+}
+export { NavBar }
